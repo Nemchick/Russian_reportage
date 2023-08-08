95b57cebe61deba3f7cff30399c5699d433c2000
Nemchick <cirillgoos@mail.ru>
2023-08-08 20:36:38 +0600

    Увольнение после этого коммита

--- js/test.js
+++ js/test.js
@@ -1,30 +1,43 @@
-var controller = new ScrollMagic.Controller(); 
+var controller = new ScrollMagic.Controller();
 
-// container pin
 var startpin = new ScrollMagic.Scene({
-  triggerElement: ".container_text", // Указываем, что скролл нужно считать внутри блока .container_text
-  duration: 200 // Длительность сцены в пикселях (от начала до конца сцены)
+  triggerElement: ".container_text",
+  duration: 10
 })
-.setPin(".container_text") // Закрепление элемента с классом ".container_text" во время сцены
-.addTo(controller); // Добавление сцены к контроллеру
-
-// tween
-var tl = new TimelineMax(); 
-
-tl.add(
-	TweenMax.to(".svg-container", 1, {scale: 10, transformOrigin: "50%, 50%", ease:Power2.easeInOut}).delay(1), 
-  )
-  
-  tl.add( 
-	TweenMax.to(".svg-container", 1, {opacity: 0, ease:Power2.easeInOut}), // Задержка в 1 секунду
-  )
-
-new ScrollMagic.Scene({
-  triggerElement: ".container_text", // Указываем, что скролл нужно считать внутри блока .container_text
-  duration: 1400// Длительность сцены в пикселях (от начала до конца сцены)
-})
-.setTween(tl) // Привязка анимационной последовательности к сцене
+.setPin(".container_text")
 .addTo(controller);
+
+var tl = new TimelineMax();
+tl.add(
+  TweenMax.to(".svg-container", 1, {scale: 10, transformOrigin: "50%, 50%", ease: Power2.easeInOut}).delay(1)
+);
+tl.add(
+  TweenMax.to(".svg-container", 1, {opacity: 0, ease: Power2.easeInOut})
+);
+
+var scene = new ScrollMagic.Scene({
+  triggerElement: ".container_text",
+  duration: 550,
+  offset: function() {
+    return -window.innerHeight * 2;
+  }
+})
+.setTween(tl)
+.addTo(controller);
+
+// Следование блока за экраном
+scene.on("progress", function (e) {
+  var progress = e.progress;
+  var windowHeight = window.innerHeight;
+  var containerTextHeight = document.querySelector(".container_text").offsetHeight;
+  var offset = windowHeight - containerTextHeight;
+  var translateY = offset * progress;
+  document.querySelector(".container_text").style.transform = "translateY(" + translateY + "px)";
+});
+
+
+
+
 
 
 
