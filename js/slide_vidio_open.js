f8a6f632ac66104e2595a62257cf4349d5aa7fbe
Nemchick <cirillgoos@mail.ru>
2023-08-11 18:18:55 +0600

    видео

---
+++ js/slide_vidio_open.js
@@ -0,0 +1,23 @@
+const slideVidio = document.querySelector('.slide_vidio_slide_2');
+
+// Добавляем обработчик события click
+slideVidio.addEventListener('click', function(event) {
+  event.preventDefault(); // Предотвращаем переход по ссылке
+  openVideo(); // Открываем видео
+});
+
+// Функция для открытия видео
+function openVideo() {
+  const videoUrl = 'https://youtu.be/CF34727AxJA'; // Ссылка на YouTube видео с включенной автозапуском
+  const videoIframe = document.getElementById('video-iframe');
+  
+  videoIframe.setAttribute('src', videoUrl); // Устанавливаем ссылку на видео в атрибут src iframe
+  $.fancybox.open({
+    src: '#video-container',
+    type: 'inline',
+    touch: false, // Отключаем жесты прокрутки на мобильных устройствах
+    afterClose: function() {
+      videoIframe.setAttribute('src', ''); // Обнуляем ссылку на видео после закрытия модального окна
+    }
+  });
+}
