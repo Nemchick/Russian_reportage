0625a607b28b8309f7abd0200c197f1baa331323
Nemchick <cirillgoos@mail.ru>
2023-08-18 18:14:19 +0600

    первая волна правок

--- js/owl_carousel_film.js
+++ js/owl_carousel_film.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
-    $(".box_carousel_film_info").owlCarousel({
-      loop: true,
-      nav: false,
-      dots: false,
-      responsive: {
-        0: {
-          items: 1,
-        },
-        576: {
-          items: 2,
-        }
+  $(".box_carousel_film_info").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    startPosition: 1,
+    responsive: {
+      0: {
+        items: 1.15,
+      },
+      576: {
+        items: 2,
+      },
+      1400: {
+        items: 2.4,
       }
-    });
+    }
   });
+});
